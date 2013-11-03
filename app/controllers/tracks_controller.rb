1927ab0ea8b47b464191f10325d70566373981b4
dankeezer <dankeezer@gmail.com>
2013-11-02 21:43:54 -0400

    figured out auto populate

--- app/controllers/tracks_controller.rb
+++ app/controllers/tracks_controller.rb
@@ -2,7 +2,6 @@ class TracksController < ApplicationController
 
   def index
     @tracks = params[:q] ? Track.search_for(params[:q]) : Track.all
-    #get_from_soundcloud
   end
 
   def new
@@ -13,19 +12,13 @@ class TracksController < ApplicationController
   	@track = Track.find(params[:id])
   end
 
-  def save_soundcloud_track track
-    Track.create title: track[:title], stream_url: track[:stream_url]
-  end
-
   def create
   	@track = Track.new(track_params)
   	if @track.save
   		
       client = SoundCloud.new(:client_id => "284a0193e0651ff008b8d9fe6066e137")
       @sc_track = client.get('/resolve', :url => @track[:original_url])
-      t = {title: @sc_track["title"], stream_url: @sc_track["stream_url"] + "?client_id=284a0193e0651ff008b8d9fe6066e137"}
-      save_soundcloud_track(t)
-
+      @track.update(title: @sc_track["title"], stream_url: @sc_track["stream_url"] + "?client_id=284a0193e0651ff008b8d9fe6066e137")
       redirect_to @track
 
   	else
@@ -52,25 +45,6 @@ class TracksController < ApplicationController
     @track.destroy
     redirect_to tracks_path
   end
-
-
-
-
-  # def get_from_soundcloud
-  #   def find_missing_stream
-  #     Track.last[:original_url]
-  #   end
-  #   client = SoundCloud.new(:client_id => "284a0193e0651ff008b8d9fe6066e137")
-  #   @sc_track = client.get('/resolve', :url => find_missing_stream)
-  #   t = {title: @sc_track["title"], stream_url: @sc_track["stream_url"]}
-  #   save_soundcloud_track(t)
-  # end
-
-
-
-  # def save_soundcloud_track track
-  #     Track.create title: track[:title], stream_url: track[:stream_url]
-  # end
 
   private
 
