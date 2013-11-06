f6a54bdcc9a3711f8f0d1b80d53a9c1da0c5f57a
dankeezer <dankeezer@gmail.com>
2013-11-05 20:39:28 -0500

    this is a stupid way to get something to work

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,6 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
-
   attr_accessible :email, :password, :password_confirmation
 
   has_many :tracks
