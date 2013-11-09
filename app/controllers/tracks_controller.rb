737d070ce49143a542b09b9faac8ff61471189ab
dankeezer <dankeezer@gmail.com>
2013-11-08 22:35:28 -0500

    still playing with user ids

--- app/controllers/tracks_controller.rb
+++ app/controllers/tracks_controller.rb
@@ -1,8 +1,14 @@
 class TracksController < ApplicationController
 
+# create Parties based on sessions? When a User clicks to PLAY a Track that Track is 
+# stored (in order) in a Party playlist that defaults to date.
+# e.g. http://danceparty911.com/tracks/dankeezer/Nov082013
+# then you can determine live playlist building.
+
   def index
     @tracks = params[:q] ? Track.search_for(params[:q]) : Track.all(:order => "created_at DESC")
-    @user = current_user.id
+    #@track.user_id = current_user.id
+    #@tracks = current_user.tracks
   end
 
   def user
@@ -21,7 +27,7 @@ class TracksController < ApplicationController
     if params[:track][:original_url] == "up down left right a b start"
       @secret_code_data = Track.set_secret_playlist
       @secret_code_data.each do |data|
-        track = Track.new(title: data[:title], stream_url: data[:stream_url], artist_name: data[:artist_name], user_id: current_user.id)
+        track = Track.new(title: data[:title], stream_url: data[:stream_url], artist_name: data[:artist_name])
         unless track.save
           errors << "Unable to save #{data[:title]}"
         end
@@ -39,10 +45,14 @@ class TracksController < ApplicationController
       end
       errors = []
       @soundcloud_data.each do |data|
-        track = Track.new(title: data[:title], stream_url: data[:stream_url], artist_name: data[:artist_name], user_id: current_user.id)
-                raise
-        unless track.save
-          errors << "Unable to save #{data[:title]}"
+        track = Track.new(title: data[:title], stream_url: data[:stream_url], artist_name: data[:artist_name], original_url: params[:track][:original_url])
+          if defined? current_user
+            Track.update(user_id: current_user.id)
+          else
+            Track.update(user_id: Time.now)
+          end
+         unless track.save
+         errors << "Unable to save #{data[:title]}"
         end
 
       end
