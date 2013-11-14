27ee0fda1a499b6532363a5623a8690849519f9e
dankeezer <dankeezer@gmail.com>
2013-11-14 18:48:24 -0500

    old page works on old

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,9 @@ Danceparty911V2::Application.routes.draw do
   get 'welcome' => 'welcome#index'
   get 'dj' => 'dj#index'
   get 'login' => 'login#index'
+  get 'old' => 'old_page#index'
 
   # get 'new' => 'tracks#index'
   get ':username', to: 'dj#user'
+
 end
