7031da77d2340fbdb23bf7423200a87966464f48
dankeezer <dankeezer@gmail.com>
2013-11-13 19:45:33 -0500

    added client id back into initializer for heroku

--- config/initializers/soundcloud.rb
+++ config/initializers/soundcloud.rb
@@ -1,1 +1,2 @@
-SOUNDCLOUD_CLIENT = SoundCloud.new(:client_id => ENV["SOUNDCLOUD_CLIENT_ID"])
+#SOUNDCLOUD_CLIENT = SoundCloud.new(:client_id => ENV["SOUNDCLOUD_CLIENT_ID"])
+SOUNDCLOUD_CLIENT = SoundCloud.new(:client_id => "310e867035eacd04d104cedd5705b31e")
